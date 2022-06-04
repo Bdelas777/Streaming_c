e6cf1643c4ffa323c6e20bcc1cf9834bfaa5082f
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2022-06-04 12:25:08 -0500

    FInal Avance 2
    Este es la parte final del avance 2

--- Reto/Proyecto/src/Peliculas.cpp
+++ Reto/Proyecto/src/Peliculas.cpp
@@ -10,7 +10,7 @@ Peliculas::Peliculas(string _iD, string _titulo, int _duracion,string _genero, d
 //Setters
 void Peliculas::setPtrPelicula(Pelicula *_pelicula)
 {
-	if (cantidad <50){
+	if (cantidad < 50){
 		arrPtrPeliculas[cantidad++] = _pelicula;
 	}
 }
@@ -43,21 +43,12 @@ void Peliculas::leerArchivo()
 	cantidad = 0;
 
 	while(getline(fin,line)){
-		//cout << cantidad << line << endl;
 		stringstream s(line);
 		int iR = 0;
 		while(getline(s,word,',')){
-			row[iR++]=word;
+			row[iR++] = word;
 		}
-		arrPtrPeliculas[cantidad]= new Pelicula(row[0],row[1] ,stoi(row[2]),row[3],stod(row[4]),stoi(row[5]));
-		/*cout << "Id: "<< row[0] << "\n";
-		cout << "Titulo: "<< row[1] << "\n";
-		cout << "Duracion: "<< row[2] << "\n";
-		cout << "Genero: "<< row[3] << "\n";
-		cout << "Calificacion: "<< row[4] << "\n";
-		cout << "Oscares: "<< row[5] << "\n";
-		cout <<"Dentro del objeto: "<< arrPtrPeliculas[cantidad]->str() << endl;*/
-		cantidad= cantidad +1;
+		setPtrPelicula(new Pelicula(row[0],row[1] ,stoi(row[2]),row[3],stod(row[4]),stoi(row[5])));
 
 	}
 	fin.close();
@@ -65,6 +56,7 @@ void Peliculas::leerArchivo()
 		cout<< iR << "-"<< arrPtrPeliculas[iR]->str()<<endl;
 	}*/
 }
+
 void Peliculas::reporteTodasLasPeliculas()
 {
 	double acumulado=0;
@@ -82,28 +74,20 @@ void Peliculas::reporteTodasLasPeliculas()
     else
         cout<<0;
 }
+
 void Peliculas::reportePorCalificacion(double _calificacion)
 {
-	int count=0;
 	for(int index=0;index<cantidad;index++){
-			if(arrPtrPeliculas[index]->getCalificacion()==_calificacion){
-				count++;
+			if(arrPtrPeliculas[index]->getCalificacion()==_calificacion)
 				cout<<arrPtrPeliculas[index]->str()<<endl;
-			}
-
 	}
-	if( count==0)
-		cout<<"No hay peliculas del calificacion: "<< _calificacion;
 }
-void Peliculas::reporteGenero(string genero)
+
+
+void Peliculas::reporteGenero(string _genero)
 {
-	int contador=0;
 	for(int index=0;index<cantidad;index++){
-			if(arrPtrPeliculas[index]->getGenero()==genero){
+			if(arrPtrPeliculas[index]->getGenero()==_genero)
 				cout<<arrPtrPeliculas[index]->str()<<endl;
-				contador++;
-			}
 	}
-	if( contador==0)
-		cout<<"No hay peliculas del genero: "<< genero;
 }
